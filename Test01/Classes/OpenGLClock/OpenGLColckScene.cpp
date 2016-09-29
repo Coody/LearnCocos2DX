1004c8efc04476a3bfc61d22f0a46b8c5cfbf87d
Coody <coody0829@gmail.com>
2016-09-29 17:13:39 +0800

    [ chore ] 加入 openGL 的 clock scene

---
+++ Test01/Classes/OpenGLClock/OpenGLColckScene.cpp
@@ -0,0 +1,24 @@
+//
+//  OpenGLColckScene.cpp
+//  Test01
+//
+//  Created by Coody on 2016/9/29.
+//
+//
+
+#include "OpenGLColckScene.h"
+
+USING_NS_CC;
+
+bool OpenGLClockLayer::init(){
+    
+    if ( CCLayerColor::create() ) {
+        
+        
+        
+        return true;
+    }
+    
+    return false;
+    
+}
---
+++ Test01/Classes/OpenGLClock/OpenGLColckScene.h
@@ -0,0 +1,30 @@
+//
+//  OpenGLColckScene.h
+//  Test01
+//
+//  Created by Coody on 2016/9/29.
+//
+//
+
+#ifndef OpenGLColckScene_h
+#define OpenGLColckScene_h
+
+#include "cocos2d.h"
+
+class OpenGLClockLayer : public cocos2d::CCLayerColor
+{
+public:
+    
+    virtual bool init();
+    
+    static cocos2d::CCScene *scene();
+    
+    CREATE_FUNC(OpenGLClockLayer);
+    
+    
+private:
+    
+    
+};
+
+#endif /* OpenGLColckScene_hpp */
